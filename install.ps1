b4b2a3d25565252c1fb17a109b0a258312097e41
Rafanov <raihanbhineka519@gmail.com>
2026-04-24 00:41:53 +0800

    Fix Installer Brutall

--- install.ps1
+++ install.ps1
@@ -12,13 +12,21 @@ Invoke-WebRequest "$raw/main.py"   -OutFile "$dir\main.py"
 Invoke-WebRequest "$raw/chara.png" -OutFile "$dir\chara.png"
 Invoke-WebRequest "$raw/icon.ico"  -OutFile "$dir\icon.ico"
 
+$python = "python"
 if (!(Get-Command python -ErrorAction SilentlyContinue)) {
-    Write-Host "Python does not exist in your computer system, please install it first." -ForegroundColor Red
-    exit
+    if (Get-Command py -ErrorAction SilentlyContinue) {
+        $python = "py"
+    } else {
+        Write-Host "Python does not exist in your computer system, please install it first." -ForegroundColor Red
+        exit
+    }
 }
 
 Write-Host "Installing dependencies..." -ForegroundColor Yellow
-python -m pip install pillow --quiet
+
+& $python -m ensurepip --upgrade 2>$null
+
+& $python -m pip install pillow --quiet
 
 Write-Host "Launching jouko..." -ForegroundColor Green
-Start-Process python -ArgumentList "$dir\main.py" -WindowStyle Hidden
+Start-Process $python -ArgumentList "$dir\main.py" -WindowStyle Hidden
